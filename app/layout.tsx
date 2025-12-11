e862cb77ec0f92db6a679938eeac0dfbed4a40a0
Gabriel Tintarescu <gabriel@Gabriels-MacBook-Pro-M4.local>
2025-12-11 10:35:47 +0200

    feat: enable vercel SpeedInsight v

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
@@ -70,6 +71,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
